dc94adf0705e74d105622dcb4d70c1cd8ca42c35
Sigurd Tullander <2935944-sigurdo@users.noreply.gitlab.com>
2024-03-19 22:25:02 +0100

    Shortcuts to useful telescope commands

--- .config/nvim/keymaps.lua
+++ .config/nvim/keymaps.lua
@@ -1,11 +1,17 @@
 
 
 wk.register({
-    v = { "<C-S-v>", "Visual block mode"},
-    b = { ":Explore<CR>", "Browser (netrw)"},
-    e = { ":NvimTreeFocus<CR>", "Explorer"},
-    E = { ":NvimTreeClose<CR>", "Close explorer"},
-    f = { ":Telescope find_files<CR>", "Find file"},
+    v = { "<C-S-v>", "Visual block mode" },
+    e = { "<cmd>NvimTreeFindFile<CR>", "Explorer" },
+    E = { "<cmd>NvimTreeClose<CR>", "Close explorer" },
+    f = { "<cmd>Telescope find_files<CR>", "Find file" },
+    F = { "<cmd>Telescope live_grep<CR>", "Find pattern" },
+    b = { "<cmd>Telescope buffers<CR>", "Buffers" },
+    m = { "<cmd>Telescope marks<CR>", "Marks" },
+    l = {
+        name = "LSP",
+        d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Jump to definition" },
+    },
 }, { prefix = "<leader>"}
 )
 
